5084c479c056df6172eec85dea2a1a0c0325d5a4
Toqa-Khalifa <toqa.khalifa99@gmail.com>
2025-09-14 22:34:26 +0300

    adding task number 6

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+// Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You must use a loop (while, for, etc.)
+
+let arr =["C is fun","Python is cool","JavaScript is amazing"];
+
+for(let i =0; i<arr.length; i++ ){
+  console.log(arr[i]);
+}

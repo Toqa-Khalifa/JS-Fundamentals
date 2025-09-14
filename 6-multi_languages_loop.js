3edd6aebb4ddf2bc5ef2f769d52bea09d847178a
Toqa-Khalifa <toqa.khalifa99@gmail.com>
2025-09-14 23:09:24 +0300

    correcting the code

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,13 +1,6 @@
-// Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
-
-// The first line: “C is fun”
-// The second line: “Python is cool”
-// The third line: “JavaScript is amazing”
-// You must use console.log(...) to print all output
-// You must use a loop (while, for, etc.)
 
 const arr =["C is fun","Python is cool","JavaScript is amazing"];
 
-for(let i =0; i<arr.length; i++ ){
+for (let i =0; i<arr.length; i++ ){
   console.log(arr[i]);
 }

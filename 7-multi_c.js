c3dbea47a191d8e9bbc265cbe433f7a85f235942
Toqa-Khalifa <toqa.khalifa99@gmail.com>
2025-09-14 23:51:42 +0300

    adding task num 7

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const { argv } = require('node:process');
+
+const arg = parseInt (argv[2]);
+if (arg){
+  for (i = 0; i<arg; i++){
+    console.log("C is fun");
+  } 
+} else {
+    console.log("Missing number of occurrences");
+  }
